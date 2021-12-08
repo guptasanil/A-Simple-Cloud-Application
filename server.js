decfa01e2c4cfc0f2778a2c1602bf74990319f1f
guptasanil <guptasanil2504@gmail.com>
2021-12-08 17:42:43 +0000

    cors

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
+var cors = require("cors");
 require("dotenv").config();
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 const app = express();
 
 const port = 3000;
-const path = require("path");
-// const { info } = require("console");
-let publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath));
-// app.get('/createtable', createTable)
+app.use(cors());
+// const path = require("path");
+// // const { info } = require("console");
+// let publicPath = path.resolve(__dirname, "public");
+// app.use(express.static(publicPath));
+// // app.get('/createtable', createTable)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 AWS.config.update({
